d9889ce38b4b57f8fb1c0b03302e660918402144
magdalenawachowska <232483@student.uek.krakow.pl>
2024-05-27 17:55:41 +0200

    2024-05-27 MW

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,10 @@ import numpy as np
 import requests
 import json
 import os
+import io
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, send_file
 
 @app.route('/')                  #sciezki - wywolana zostanie funkcja ponizej
 def index():
@@ -27,6 +28,7 @@ def extract():
             page_dom = BeautifulSoup(response.text, "html.parser")
             opinions_count = utils.extract(page_dom,"a.product-review__link > span")
             if opinions_count:
+                product_name = utils.extract(page_dom,"h1")
                 url = f"https://www.ceneo.pl/{product_id}/opinie-1"
                 all_opinions = []
                 while (url):
@@ -53,7 +55,23 @@ def extract():
                         json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
                     opinions = pd.DataFrame.from_dict(all_opinions)
 
-                #proces ekstrakcji
+                    opinions.rating = opinions.rating.apply(lambda rate: rate.split("/")[0].replace(",",".")).astype(float)
+                    
+                    product = {
+                        "product_id" : product_id,
+                        "product_name": product_name,
+                        "opinions_count": opinions.shape[0],
+                        "pros_count": int(opinions.pros.astype(bool).sum()),          
+                        "cons_count": int(opinions.cons.astype(bool).sum()),
+                        "average_rating": opinions.rating.mean(),
+                        "rating_distribution": opinions.rating.value_counts().reindex(np.arange(0,5.5,0.5), fill_value=0.0).to_dict(),
+                        "recommendation_distribution":opinions.recommendation.value_counts(dropna=False).reindex(["Polecam", "Nie polecam", None]).to_dict()
+                    }
+                    if not os.path.exists("app/data/products"):
+                        os.mkdir("app/data/products")
+                    with open(f"app/data/products/{product_id}.json", "w", encoding="UTF-8") as jf:
+                        json.dump(product, jf, indent=4, ensure_ascii=False)
+                
                 return redirect(url_for('product', product_id= product_id))            
             return render_template("extract.html.jinja", error="Dla produktu o podanym kodzie nie ma opinii")
         return render_template("extract.html.jinja", error="Produkt o podanym kodzie nie istnieje")
@@ -61,8 +79,12 @@ def extract():
 
 @app.route('/products')                 
 def products():
-    products = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
-    return render_template("products.html.jinja")
+    products_list = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
+    products = []
+    for product_id in products_list:
+        with open(f"app/data/products/{product_id}.json", "r", encoding="UTF-8") as jf:
+            products.append(json.load(jf))
+    return render_template("products.html.jinja", products = products)
 
 @app.route('/author')                 
 def author():
@@ -71,3 +93,17 @@ def author():
 @app.route('/product/<product_id>')
 def product(product_id):
     return render_template("product.html.jinja", product_id= product_id)
+
+@app.route('/product/download_json/<product_id>')
+def download_json(product_id):
+    return send_file(f"data/products/{product_id}.json", "text/json", as_attachment=True)
+
+@app.route('/product/download_csv/<product_id>')
+def download_csv(product_id):
+    opinions = pd.read_json(f"app/data/opinions/{product_id}.json")
+    buffer = io.BytesIO(opinions.to_csv(sep=";", decimal=",", index=False).encode())       #przesylamy do dynamicznego pliku- ma byc utworzony tylko w pamieci,a nie na dysku
+    return send_file(buffer, "text/csv", as_attachment=True, download_name=f"{product_id}.csv")
+
+@app.route('/product/download_xlsx/<product_id>')
+def download_xlsx(product_id):
+    pass
